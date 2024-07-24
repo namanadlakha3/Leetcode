3cd9d1834fd1b8a1897e9a6a70cc093e8bdef8b7
Naman Adlakha <naman.adlakha3@gmail.com>
2024-07-24 17:06:41 +0530

    Time: 2646 ms (5.19%) | Memory: 77.8 MB (90.48%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int covertValue(vector<int>& mapping, int num){
+          long int newNum = 0;
+          string s = to_string(num);
+
+          for(int i=0;i<s.size();i++){
+           s[i] = mapping[int(s[i])-48] + '0';
+          }
+      
+            return stoi(s);
+    }
+
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end(),[&](int a,int b){
+              return covertValue(mapping,a) < covertValue(mapping,b);
+        });
+        return nums;
+    }
+};
