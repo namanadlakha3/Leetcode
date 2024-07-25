bcdadc2730317bceeab0b7047190ce9425068b95
Naman Adlakha <naman.adlakha3@gmail.com>
2024-07-26 00:44:53 +0530

    Time: 130 ms (61.91%) | Memory: 69.4 MB (84.29%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    void heapify(vector<int> &nums, int i){
+    void heapify(vector<int> &nums, int i, int n){
         int index = i;
         int left = 2*i + 1;
         int right = 2*i + 2;
-        int n = nums.size();
 
         if(left<n && nums[left]>nums[index]) index = left;
         if(right<n && nums[right]>nums[index]) index = right;
 
         if(index!=i){
             swap(nums[i],nums[index]);
-            heapify(nums,index);
+            heapify(nums,index,n);
         } 
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        unordered_map<int,int>mp;
+       int n = nums.size();
 
-        int low=INT_MAX, high = INT_MIN;
-        for(int &num: nums){
-            mp[num]++;
-            low = min(low,num);
-            high = max(high,num);
-        }
-        
-        int j = 0;
-        for(int i = low;i<=high;i++){
-            while(mp[i]!=0){
-               nums[j++] = i;
-               mp[i]--;
-            }
-        }
+       // Build heap
+       for(int i = n/2; i>=0;i--){
+        heapify(nums,i,n);
+       }
 
-        return nums;
+       for(int i=n-1;i>=0;i--){
+        swap(nums[0],nums[i]);
+        heapify(nums,0,i);
+       }
 
+
+       return nums;
     }
 };
