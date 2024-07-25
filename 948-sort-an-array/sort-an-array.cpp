d87a7005082424d002ba7b9feaa25fdaf82f455a
Naman Adlakha <naman.adlakha3@gmail.com>
2024-07-26 00:41:13 +0530

    Time: 170 ms (54.72%) | Memory: 98.2 MB (46.08%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    void heapify(vector<int> &nums, int i){
+        int index = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int n = nums.size();
+
+        if(left<n && nums[left]>nums[index]) index = left;
+        if(right<n && nums[right]>nums[index]) index = right;
+
+        if(index!=i){
+            swap(nums[i],nums[index]);
+            heapify(nums,index);
+        } 
+    }
+
     vector<int> sortArray(vector<int>& nums) {
         unordered_map<int,int>mp;
 
