cc40a0ed869d3a0c92cc7d84c1cb4731683977db
Naman Adlakha <naman.adlakha3@gmail.com>
2024-08-18 00:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (25.08%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,6 +1,6 @@
 
 class Solution {
-    public int trap(int[] height) {
+    private int dynamicProgramming(int[] height){
         int n = height.length;
         int ans = 0;
 
@@ -22,5 +22,28 @@ class Solution {
         }
 
         return ans;
+    }
+
+    int twoPointer(int[] height){
+          int left = 0, right = height.length - 1;
+          int ans = 0, leftMax = 0, rightMax = 0;
+
+          while(left < right){
+             if(height[left]<height[right]){
+                leftMax = Math.max(leftMax,height[left]);
+                ans+= leftMax - height[left];
+                ++left;
+             }
+             else {
+                rightMax = Math.max(rightMax,height[right]);
+                ans+= rightMax - height[right];
+                --right;
+             }
+          }
+
+          return ans; 
+    }
+    public int trap(int[] height) {
+        return twoPointer(height);
     }
 }
