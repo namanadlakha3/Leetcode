2a2086407ebc0bc9f5f0fbc1fde1505f64a1386f
Naman Adlakha <naman.adlakha3@gmail.com>
2024-08-17 18:46:48 +0530

    Time: 6 ms (68.96%) | Memory: 44.5 MB (54.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -4,9 +4,7 @@ class Solution {
         HashSet<Character> chars = new HashSet<>();
 
         int startIndex = 0;
-        // abcaabb
-        //int endIndex = s.length() - 1;
-// startIndex = 0, lastIndex = 0;
+
         for(int i=0;i<s.length();i++){
             while(chars.contains(s.charAt(i))){
                 chars.remove(s.charAt(startIndex));
