d2dee77197ee131cab5b20de9f422b21eafefc17
Naman Adlakha <naman.adlakha3@gmail.com>
2024-08-19 22:00:32 +0530

    Time: 18 ms (82.75%) | Memory: 12.3 MB (80.43%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int count = 0;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')++count;
+            else if(s[i] == ')' && count == 0) s[i]='.';
+            else if(s[i] == ')' && count>0) --count;
+        }
+
+        for(int i=s.size()-1;i>=0;i--){
+            if(count>0 && s[i]=='('){
+                --count;
+                s[i]='.';
+            }
+        }
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='.')ans.push_back(s[i]);
+        }
+
+        
+        return ans;
+    }
+};
