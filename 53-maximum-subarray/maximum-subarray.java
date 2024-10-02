f93b53a793de3440f89904ebe9fb1dcbefbd3f3a
Naman Adlakha <naman.adlakha3@gmail.com>
2024-10-02 22:38:02 +0530

    Time: 1 ms (99.41%) | Memory: 56.8 MB (69.99%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0, maxSum=Integer.MIN_VALUE;
+        for(int i=0 ; i<nums.length; i++){
+            sum+=nums[i];
+            if(maxSum<sum){
+                maxSum = sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+}
