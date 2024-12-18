1eaf3b0498bbf9fd2cc252a27e399fcec454299b
Naman Adlakha <naman.adlakha3@gmail.com>
2024-12-18 23:47:30 +0530

    Time: 6 ms (23.49%) | Memory: 42.9 MB (11.70%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -4,17 +4,18 @@ class Solution {
         int size1= word1.length(), size2 = word2.length();
         int i=0,j=0;
 
-        while(i<size1 && j<size2){
-            word3 = word3 + word1.charAt(i)+ word2.charAt(j);
-            i++;j++;
-        }
-        if(word3.length()!= (size1+size2)){
-            if(i==size1){
-                word3 = word3 + word2.substring(j);
-            } else{
-                word3 = word3 + word1.substring(i);
+        while(i<size1 || j<size2) {
+            if(i<size1){
+                word3 = word3 + word1.charAt(i);
+                i++;
             }
+            if(j<size2){
+                word3 = word3 + word2.charAt(j);
+                j++;
+            }
+
         }
+
         
         return word3;
     }
