41aee27ed9bf0d6788a15e13efdf2ab7000f4708
Naman Adlakha <naman.adlakha3@gmail.com>
2025-01-23 19:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (82.08%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 1, ans = nums[0], lastMx = 0;
+        int count = 1, ans = nums[0];
 
         for(int i = 1;i<nums.size();i++){
-            cout<<count;
-               if(count==0){
-                ans = nums[i];
-               }
-
+            if(count==0) ans = nums[i];
             if(ans == nums[i])++count;
             else --count;
-
- cout<<count<<endl;
-            if(count==0){
-                ans = nums[i];
-               }
         }
         
         return ans;
